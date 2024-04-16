049abdecdbe188f78800c517a9945f744d2cfb1e
Sympathy34 <127864602+Sympathy34@users.noreply.github.com>
2024-04-16 16:57:17 -0300

    1 inteiracao da requisição

---
+++ codigo/Requisição.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Requisição
+{
+    class Requisicao
+    {
+        private DateTime dataEntrada;
+        private DateTime dataSaida;
+        private Cliente cliente;
+        private int quantPessoas;
+        private Mesa mesa;
+        public Requisicao(cliente cliente, int quantPessoas)
+        {
+            this.cliente = cliente;
+            this.quantPessoas = quantPessoas;
+        }
+
+        public void registrarEntrada(DateTime dataEntrada)
+        {
+            this.dataEntrada = DateTime.Now;
+        }
+        public void registrarSaida(DateTime dataSaida)
+        {
+            dataSaida = DateTime.Now;
+        }
+        public Mesa reservar (Mesa mesa)
+        {
+            mesa.alternarStatus();
+            return mesa;
+        }
+        public bool alternarStatus()
+        {
+            if(statusReserva()==true)
+            {
+                return false;
+            }
+            return true;
+        }
+        public Mesa obterMesa()
+        {
+            return mesa;
+        }
+        public bool statusReserva()
+        {
+            return mesa.verificarDisponivel();
+        }
+        public bool verificarStatus()
+        {
+           return statusReserva();
+        }
+        public int obterQuantidade()
+        {
+            return mesa.ObterCapacidade();
+        }
+    }
+}
